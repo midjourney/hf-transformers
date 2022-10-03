0073ae2f4f5b6999a976dd812e0f4dc9e635a53f
jack <jack@gallabytes.com>
2022-10-03 23:04:29 +0000

    drop useless position id attributes that break gda state

--- src/transformers/models/clip/modeling_flax_clip.py
+++ src/transformers/models/clip/modeling_flax_clip.py
@@ -215,7 +215,6 @@ class FlaxCLIPVisionEmbeddings(nn.Module):
         self.num_patches = (image_size // patch_size) ** 2
         num_positions = self.num_patches + 1
         self.position_embedding = nn.Embed(num_positions, embed_dim, embedding_init=jax.nn.initializers.normal())
-        self.position_ids = jnp.expand_dims(jnp.arange(0, num_positions, dtype="i4"), axis=0)
 
     def __call__(self, pixel_values):
         patch_embeds = self.patch_embedding(pixel_values)
@@ -225,7 +224,9 @@ class FlaxCLIPVisionEmbeddings(nn.Module):
         class_embeds = jnp.expand_dims(self.class_embedding, axis=(0, 1))
         class_embeds = jnp.tile(class_embeds, (batch_size, 1, 1))
         embeddings = jnp.concatenate([class_embeds, patch_embeds], axis=1)
-        embeddings = embeddings + self.position_embedding(self.position_ids)
+        with jax.ensure_compile_time_eval():
+            position_ids = jnp.expand_dims(jnp.arange(0, num_positions, dtype="i4"), axis=0)
+        embeddings = embeddings + self.position_embedding(position_ids)
         return embeddings
 
 
@@ -239,9 +240,6 @@ class FlaxCLIPTextEmbeddings(nn.Module):
         self.token_embedding = nn.Embed(self.config.vocab_size, embed_dim, embedding_init=jax.nn.initializers.normal())
         self.position_embedding = nn.Embed(
             self.config.max_position_embeddings, embed_dim, embedding_init=jax.nn.initializers.normal()
-        )
-        self.position_ids = jnp.expand_dims(
-            jnp.arange(0, self.config.max_position_embeddings, dtype="i4"), axis=(0, 1)
         )
 
     def __call__(self, input_ids, position_ids):
